7b9fda9ebf775431754e8377822b2f670c408237
KhiaJEO <khiajeo@gmail.com>
2022-03-15 13:23:07 +0800

    Completed step 11
    Updated project/views def post function and class Project Detail function

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from.models import Project
@@ -14,12 +16,22 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                )
 
 class ProjectDetail(APIView):
 
     def get_object(self, pk):
-        return Project.objects.get(pk=pk)
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
 
     def get(self, request, pk):
         project = self.get_object(pk)
