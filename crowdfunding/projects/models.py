a08958bf77206db5c4ecd23098921f6e1b7e641a
KhiaJEO <khiajeo@gmail.com>
2022-03-22 20:36:30 +0800

    add categories to models
    using slug field - please look that up...

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField() 
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    goal = models.IntegerField()
+    summary = models.TextField()
     image = models.URLField()
+    goal = models.IntegerField()
+    issue = models.TextField()
+    tools = models.TextField()
+    science = models.TextField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
     # owner = models.CharField(max_length=200)
@@ -14,6 +21,7 @@ class Project(models.Model):
         on_delete=models.CASCADE,
         related_name='owner_projects'
     )
+    categories = models.ManyToManyField(Category, blank = True, null = True)
     
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -25,3 +33,4 @@ class Pledge(models.Model):
         related_name='pledges'
     )
     supporter = models.CharField(max_length=200)
+
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,9 +1,11 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from.models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
+
 
 class PledgeList(APIView):
 
@@ -21,7 +23,9 @@ class PledgeList(APIView):
                 status=status.HTTP_201_CREATED
             )
 
+
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -41,11 +45,19 @@ class ProjectList(APIView):
                 status=status.HTTP_400_BAD_REQUEST
                 )
 
+
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+        ]
 
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            # return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist:
             raise Http404
 
@@ -54,4 +66,14 @@ class ProjectDetail(APIView):
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data 
+        serializer = ProjectDetailSerializer(
+            instance=project, 
+            data=data, 
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
 
