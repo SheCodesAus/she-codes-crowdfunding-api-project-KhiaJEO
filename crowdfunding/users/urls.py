27c6d3324aea50b435c87b203741d02816b362f2
KhiaJEO <khiajeo@gmail.com>
2022-04-10 19:34:55 +0800

    Updated model with make migrations
    deleted migration files related to profiles to complete make migrations and migrate

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.CustomUserList.as_view()),
-    path('<int:pk>/', views.CustomUserDetail.as_view()),
+    path('users/', views.CustomUserList.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
     path('register/', views.RegisterView.as_view()),
     # path('profile/<int:pk>/', views.ProfileDetail.as_view()),
     path('puns/', views.PunsList.as_view()),
