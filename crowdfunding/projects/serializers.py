f7029b8fd14d21daed44b0b973ca8f4b742e3072
KhiaJEO <khiajeo@gmail.com>
2022-04-26 20:35:07 +0800

    updating supporter in pledge serializer

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -8,7 +8,8 @@ class PledgeSerializer(serializers.Serializer):
     amount = serializers.IntegerField()
     comment = serializers.CharField(max_length=200)
     anonymous = serializers.BooleanField()
-    supporter = serializers.CharField(max_length=200)
+    # supporter = serializers.CharField(max_length=200)
+    support = serializers.ReadOnlyField(source='supporter.id')
     project_id = serializers.IntegerField()
 
     def create(self, validated_data):
