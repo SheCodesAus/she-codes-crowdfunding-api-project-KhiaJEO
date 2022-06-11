600cc00721ac986c739922200adc62747b868111
KhiaJEO <khiajeo@gmail.com>
2022-06-11 23:53:51 +0800

    added delete to puns

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -75,3 +75,8 @@ class PunsList(APIView):
             return Response(serializer.data)
         return Response(serializer.errors,
         status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        puns = self.get_object(pk)
+        puns.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
